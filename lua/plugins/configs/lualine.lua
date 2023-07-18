a8495501327b290fb63a79630104bcb69d7c9ca2
Chris Nguyen <chris.dungnguyen@gmail.com>
2023-07-18 07:38:00 +0700

    feat: update lualine path

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -1,15 +1,15 @@
 local colors = {
-  bg       = '#202328',
-  fg       = '#bbc2cf',
-  yellow   = '#ECBE7B',
-  cyan     = '#008080',
-  darkblue = '#081633',
-  green    = '#98be65',
-  orange   = '#FF8800',
-  violet   = '#a9a1e1',
-  magenta  = '#c678dd',
-  blue     = '#51afef',
-  red      = '#ec5f67',
+  bg = "#202328",
+  fg = "#bbc2cf",
+  yellow = "#ECBE7B",
+  cyan = "#008080",
+  darkblue = "#081633",
+  green = "#98be65",
+  orange = "#FF8800",
+  violet = "#a9a1e1",
+  magenta = "#c678dd",
+  blue = "#51afef",
+  red = "#ec5f67",
 }
 
 local config = {
@@ -21,11 +21,10 @@ local config = {
       {
         "filename",
         file_status = true, -- displays file status (readonly status, modified status)
-        path = 1,           -- 0 = just filename, 1 = relative path, 2 = absolute path
+        path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
       },
     },
   },
 }
-
 
 return config
