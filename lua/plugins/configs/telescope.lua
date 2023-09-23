36293cc3749bcfd2581be6554385a396a94d09f2
Chris Nguyen <chrisnguyen@Chriss-Mac-mini.local>
2023-09-23 07:17:08 +0700

    feat: update cheatsheet

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -37,3 +37,4 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = 'Search current Word' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = 'Search by Grep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = 'Search Diagnostics' })
+vim.keymap.set('n', '<leader>sc', '<cmd> Cheatsheet <cr>', { desc = "Search Cheatsheet" })
