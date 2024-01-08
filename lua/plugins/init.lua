563cd8203cf6cf68ac7079899570d2487a4c8b60
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-01-08 20:48:38 +0700

    feat: resolved comment

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,6 @@ return {
 			'williamboman/mason-lspconfig.nvim',
 
 			-- Useful status updates for LSP
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
 
 			-- Additional lua configuration, makes nvim stuff amazing!
