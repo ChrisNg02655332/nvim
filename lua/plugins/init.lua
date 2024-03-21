644d15453956e9cadbcdfcec6d5dd668346a104c
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-03-21 23:36:16 +0700

    feat: update noice position

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -283,6 +283,19 @@ return {
 				inc_rename = false,       -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false,   -- add a border to hover docs and signature help
 			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = 10
+					}
+				},
+				cmdline_popupmenu = {
+					relative = "editor",
+					position = {
+						row = 13,
+					},
+				}
+			}
 		},
 	},
 
