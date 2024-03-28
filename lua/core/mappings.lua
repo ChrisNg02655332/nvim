60b2c4d628188a6e2056cff0c0891adcebae8699
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-03-28 08:12:48 +0700

    feat: map quit key

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,106 +1,123 @@
 local M = {}
 
 M.general = {
-	n = {
-		-- Remap for dealing with word wrap
-		["k"] = { "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true } },
-		["j"] = { "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true } },
+  n = {
+    -- Remap for dealing with word wrap
+    ['k'] = { "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true } },
+    ['j'] = { "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true } },
 
-		["<leader>n"] = { "<cmd> enew <cr>", { desc = "New Buffer" } },
+    ['<leader>n'] = { '<cmd> enew <cr>', { desc = 'New Buffer' } },
 
-		["<tab>"] = { "<cmd> bnex <cr>", { desc = "Next Buffer" } },
-		["<S-tab>"] = { "<cmd> bprevious <cr>", { desc = "Prev Buffer" } },
-		["<C-s>"] = { "<cmd> up <cr>", { desc = "Save Buffer" } },
+    ['<tab>'] = { '<cmd> bnex <cr>', { desc = 'Next Buffer' } },
+    ['<S-tab>'] = { '<cmd> bprevious <cr>', { desc = 'Prev Buffer' } },
+    ['<C-s>'] = { '<cmd> up <cr>', { desc = 'Save Buffer' } },
 
-		["K"] = { "<cmd> move -2 <cr>", { desc = "Move line up" } },
-		["J"] = { "<cmd> move +1 <cr>", { desc = "Move line down" } },
+    ['<leader>q'] = { '<cmd> q <cr>', { desc = 'Quit without save' } },
+    ['<leader>Q'] = { '<cmd> q! <cr>', { desc = 'Force quit without save' } },
 
-		["<C-a>"] = { "ggVG", { desc = "Select all" } },
-		["<PageUp>"] = { "^", { desc = "Move the cursor to the first non-blank character of a line" } }
-	},
+    ['K'] = { '<cmd> move -2 <cr>', { desc = 'Move line up' } },
+    ['J'] = { '<cmd> move +1 <cr>', { desc = 'Move line down' } },
 
-	i = {
-		["<PageUp>"] = { "<Esc>^ | I", { desc = "Move the cursor to the first non-blank character of a line" } },
-	}
+    ['<C-a>'] = { 'ggVG', { desc = 'Select all' } },
+    ['<PageUp>'] = { '^', { desc = 'Move the cursor to the first non-blank character of a line' } },
+  },
+
+  i = {
+    ['<PageUp>'] = { '<Esc>^ | I', { desc = 'Move the cursor to the first non-blank character of a line' } },
+  },
 }
 
 M.bdelete = {
-	n = {
-		["<leader>x"] = { function() require("bufdelete").bufdelete(0, false) end, { desc = "Close Buffer" } }
-	}
+  n = {
+    ['<leader>x'] = {
+      function()
+        require('bufdelete').bufdelete(0, false)
+      end,
+      { desc = 'Close Buffer' },
+    },
+  },
 }
 
 M.lspconfig = {
-	n = {
-		["<leader>ca"] = { vim.lsp.buf.code_action, { desc = "Hover code actions" } },
-		["<leader>ds"] = { require('telescope.builtin').lsp_document_symbols, { desc = "Document Symbol" } }
-	}
+  n = {
+    ['<leader>ca'] = { vim.lsp.buf.code_action, { desc = 'Hover code actions' } },
+    ['<leader>ds'] = { require('telescope.builtin').lsp_document_symbols, { desc = 'Document Symbol' } },
+  },
 }
 
 M.telescope = {
-	n = {
-		["<leader>sb"] = { require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" } },
-		["<leader><space>"] = { function()
-			require('telescope.builtin').buffers({
-				-- entry_maker = require("core.utils.telescope").gen_from_buffer(),
-			})
-		end, { desc = '[ ] Find existing buffers' } },
+  n = {
+    ['<leader>sb'] = { require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' } },
+    ['<leader><space>'] = {
+      function()
+        require('telescope.builtin').buffers {
+          -- entry_maker = require("core.utils.telescope").gen_from_buffer(),
+        }
+      end,
+      { desc = '[ ] Find existing buffers' },
+    },
 
-		["<leader>gf"] = { require("telescope.builtin").git_files, { desc = "Search Git Files" } },
-		["<leader>sf"] = { require("telescope.builtin").find_files, { desc = "Search Files" } },
-		["<leader>sg"] = { require("telescope.builtin").live_grep, { desc = "Search Grev" } },
-		["<leader>sh"] = { require("telescope.builtin").help_tags, { desc = "Search Help" } },
-		["<leader>sw"] = { require("telescope.builtin").grep_string, { desc = "Search current Word" } },
-		["<leader>sd"] = { require("telescope.builtin").diagnostics, { desc = "Search Diagnostics" } },
-		["<leader>sc"] = { "<cmd> Cheatsheet <cr>", { desc = "Search Cheatsheet" } },
-	}
+    ['<leader>gf'] = { require('telescope.builtin').git_files, { desc = 'Search Git Files' } },
+    ['<leader>sf'] = { require('telescope.builtin').find_files, { desc = 'Search Files' } },
+    ['<leader>sg'] = { require('telescope.builtin').live_grep, { desc = 'Search Grev' } },
+    ['<leader>sh'] = { require('telescope.builtin').help_tags, { desc = 'Search Help' } },
+    ['<leader>sw'] = { require('telescope.builtin').grep_string, { desc = 'Search current Word' } },
+    ['<leader>sd'] = { require('telescope.builtin').diagnostics, { desc = 'Search Diagnostics' } },
+    ['<leader>sc'] = { '<cmd> Cheatsheet <cr>', { desc = 'Search Cheatsheet' } },
+  },
 }
 
 M.comment = {
-	n = {
-		["<leader>/"] = {
-			function() require('Comment.api').toggle.linewise.current() end,
-			{ desc = 'Toggle Comment' } },
-	},
+  n = {
+    ['<leader>/'] = {
+      function()
+        require('Comment.api').toggle.linewise.current()
+      end,
+      { desc = 'Toggle Comment' },
+    },
+  },
 
-	v = {
-		["<leader>/"] = {
-			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", { desc = 'Toggle Comment' }
-		}
-	}
+  v = {
+    ['<leader>/'] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+      { desc = 'Toggle Comment' },
+    },
+  },
 }
 
 M.gitsigns = {
-	n = {
-		["<leader>gb"] = { "<cmd> Gitsigns blame_line<cr>", { desc = "Blame line" } },
-		["<leader>gh"] = { "<cmd> Gitsigns preview_hunk <cr>", { desc = "Preview hunk" } },
-	}
+  n = {
+    ['<leader>gb'] = { '<cmd> Gitsigns blame_line<cr>', { desc = 'Blame line' } },
+    ['<leader>gh'] = { '<cmd> Gitsigns preview_hunk <cr>', { desc = 'Preview hunk' } },
+  },
 }
 
 M.neotree = {
-	n = {
-		["<leader>e"] = { "<cmd> Neotree toggle <cr>", { desc = 'Toggle Explorer' } },
-		["<leader>o"] = {
-			function()
-				if vim.bo.filetype == "neo-tree" then
-					vim.cmd.wincmd("p")
-				else
-					vim.cmd.Neotree("focus")
-				end
-			end,
-			{ desc = "Toggle Explorer" } }
-	}
+  n = {
+    ['<leader>e'] = { '<cmd> Neotree toggle <cr>', { desc = 'Toggle Explorer' } },
+    ['<leader>o'] = {
+      function()
+        if vim.bo.filetype == 'neo-tree' then
+          vim.cmd.wincmd 'p'
+        else
+          vim.cmd.Neotree 'focus'
+        end
+      end,
+      { desc = 'Toggle Explorer' },
+    },
+  },
 }
 
 M.toggleterm = {
-	n = {
-		["<leader>gg"] = {
-			function()
-				require("core.utils").toggle_term_cmd("lazygit")
-			end, { desc = "ToggleTerm LazyGit" }
-		},
-		["<C-/>"] = { "<cmd> ToggleTerm direction=float <cr>", { desc = "ToggleTerm float" } }
-	}
+  n = {
+    ['<leader>gg'] = {
+      function()
+        require('core.utils').toggle_term_cmd 'lazygit'
+      end,
+      { desc = 'ToggleTerm LazyGit' },
+    },
+    ['<C-/>'] = { '<cmd> ToggleTerm direction=float <cr>', { desc = 'ToggleTerm float' } },
+  },
 }
 
 return M
