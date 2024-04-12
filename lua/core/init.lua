76030e55aa1d77aef014ab1b5d4c40fde2e9f98d
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-04-12 14:22:13 +0700

    fix: lint code

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set highlight on search
 vim.o.hlsearch = false
@@ -13,12 +13,12 @@ vim.o.cursorline = true
 vim.opt.scrolloff = 10
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -34,7 +34,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -50,37 +50,37 @@ vim.o.termguicolors = true
 vim.o.laststatus = 3
 
 -- Enable fold
-vim.o.foldcolumn = '0' -- '0' is not bad
+vim.o.foldcolumn = "0" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
 -- See `:help vim.keymap.set()`
 -- vim.keymap.set({'n', 'i'}, '∆', rhs)
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set('t', '<C-/>', '<C-\\><C-n><C-w>h', { silent = true })
-vim.keymap.set('i', '<C-h>', '<left>', { noremap = true })
-vim.keymap.set('i', '<C-j>', '<down>', { noremap = true })
-vim.keymap.set('i', '<C-k>', '<up>', { noremap = true })
-vim.keymap.set('i', '<C-l>', '<right>', { noremap = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("t", "<C-/>", "<C-\\><C-n><C-w>h", { silent = true })
+vim.keymap.set("i", "<C-h>", "<left>", { noremap = true })
+vim.keymap.set("i", "<C-j>", "<down>", { noremap = true })
+vim.keymap.set("i", "<C-k>", "<up>", { noremap = true })
+vim.keymap.set("i", "<C-l>", "<right>", { noremap = true })
 
 -------------------------------------- autocmds ------------------------------------------
 local autocmd = vim.api.nvim_create_autocmd
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
-autocmd('FileType', {
-  pattern = 'qf',
-  callback = function()
-    vim.keymap.set('n', '<Esc>', require('core.utils').close_qf, { desc = 'Close qf' })
-    vim.keymap.set('n', 'q', require('core.utils').close_qf, { desc = 'Close qf' })
-  end,
+autocmd("FileType", {
+	pattern = "qf",
+	callback = function()
+		vim.keymap.set("n", "<Esc>", require("core.utils").close_qf, { desc = "Close qf" })
+		vim.keymap.set("n", "q", require("core.utils").close_qf, { desc = "Close qf" })
+	end,
 })
