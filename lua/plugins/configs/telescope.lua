87ea81cae0f18e22b0c203281bf3fb975a1ebaed
Chris Nguyen <chrisnguyen@chriss-mac-mini.tailc517d.ts.net>
2024-05-02 09:29:50 +0700

    feat: ignore live_grep

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -1,10 +1,13 @@
-require("telescope").setup {
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = { "node_modules", "deps", "_build" },
+	},
 	extensions = {
-		['ui-select'] = {
-			require('telescope.themes').get_dropdown(),
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown(),
 		},
 	},
-}
+})
 
 -- Enable telescope fzf native, if installed
 pcall(require("telescope").load_extension, "fzf")
